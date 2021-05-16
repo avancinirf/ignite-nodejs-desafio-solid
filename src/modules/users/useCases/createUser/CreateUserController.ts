51cd819c2cf9dabdd391af9db812f67dd1419615
Ricardo Avancini <ravancini@PWST215s-MacBook-Pro.local>
2021-05-16 13:58:26 +0100

    Finished

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,19 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { name, email } = request.body;
+      const user = this.createUserUseCase.execute({ name, email });
+
+      return response.status(201).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        admin: user.admin,
+      });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
